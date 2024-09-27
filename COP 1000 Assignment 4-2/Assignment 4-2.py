6276979d30a31a6e19436cc2fc5ee0ff248360a4
Jagermind <jagerunz@gmail.com>
2024-09-27 14:38:29 -0400

    Initial Commit

---
+++ COP 1000 Assignment 4-2/Assignment 4-2.py
@@ -0,0 +1,18 @@
+#This will be assignment 4-2
+#Productivity = (dollarValue/ numTransacitons) / numShiftWorked
+#<=30=$50, 31-69 = $75, 70-199= $100, >=200= $200
+
+
+#declarations
+dollarValue= 0.00
+numTransactions= 0.00
+numShiftsWorked = 0.00
+employeeName="string"
+#input?
+employeeName=input("Employee Name?:")
+numShiftsWorked=int(input("How many shifts were worked?:"))
+numTransactions=int(input("How many transactions were performed?"))
+dollarValue=int(input("Total Dollar value of transactions?:"))
+#bonus calculations here
+
+#output statements here
