b0329083338eb692ad55c8f71164ddaf343c0197
Jagermind <jagerunz@gmail.com>
2024-09-27 13:31:46 -0400

    Base code is working, added if statements.

--- COP 1000 Assignment 4-1/Assignment 4-1.py
+++ COP 1000 Assignment 4-1/Assignment 4-1.py
@@ -3,12 +3,19 @@ This program calculates prices for custom house signs.
 """
 
 # Declare and initialize variables here.
-# Charge for this sign.
-# Number of characters.
-# Color of characters.
-# Type of wood.
+charge=0.00
+freeChars=5
+numChars=8
+color="gold"
+woodType="oak"
 
+charge=35.00
 # Write assignment and if statements here as appropriate.
-
+if numChars > 4:
+    charge+=4*(numChars-freeChars)
+if color=="gold":
+    charge+= 15.00
+if woodType=="oak":
+    charge+=20.00
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
