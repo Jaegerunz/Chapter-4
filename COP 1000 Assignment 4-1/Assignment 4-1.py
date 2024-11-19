ea3dcd7b24b4c64b5c7b52f447f490cd3da3341f
Jagermind <jagerunz@gmail.com>
2024-11-19 11:38:13 -0500

    Added input statements because the way it worked before bothered me. It already passed all unit tess the downloaded assignment sheet just didnt have a 3rd test on it

--- COP 1000 Assignment 4-1/Assignment 4-1.py
+++ COP 1000 Assignment 4-1/Assignment 4-1.py
@@ -5,17 +5,22 @@ This program calculates prices for custom house signs.
 # Declare and initialize variables here.
 charge=0.00
 freeChars=5
-numChars=20
-color="gold"
-woodType="pine"
+numChars=18
+color="black"
+woodType="oak"
 
 charge=35.00
+
+#input statements
+numChars=int(input("How many Characters do you need?: "))
+color=input("What color lettering do you want?: ")
+woodType=input("What wood would you like?: ")
 # Write assignment and if statements here as appropriate.
 if numChars > 5:
     charge+=4*(numChars-freeChars)
-if color=="gold":
+if color=="Gold":
     charge+= 15.00
-if woodType=="oak":
+if woodType=="Oak":
     charge+=20.00
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
